afb11d379193dd6ebedab747f902b12ac635bdb9
Daniel Hardman <daniel.hardman@gmail.com>
2024-03-05 17:48:53 +0100

    slightly prettier output
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- scripts/getroutes.py
+++ scripts/getroutes.py
@@ -2,12 +2,10 @@ import os
 import re
 import sys
 
-FOLDER_TO_SCAN = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..'))
-
 def get_routes(folder):
     routes = {}
-    folder = os.path.normpath(os.path.abspath(folder))
-    for root, dirs, files in os.walk(FOLDER_TO_SCAN):
+    folder = os.path.abspath(folder)
+    for root, _, files in os.walk(folder):
         for file_name in files:
             if file_name.endswith('.py') and 'test_' not in file_name:
                 path = os.path.join(root, file_name)
@@ -15,7 +13,7 @@ def get_routes(folder):
                 scan_file(path, relpath, routes)
     return routes
 
-ROUTE_REGEX = re.compile(r'\Wroute(?:\s*=\s*"([^"]*)"|s*\[\s*"([^"]*)"\])')
+ROUTE_REGEX = re.compile(r'\Wroute(?:\s*=\s*"([^"]*)"(?:\s+if.*\Welse\s+"([^"]*)")?|s*\[\s*"([^"]*)"\])')
 
 def scan_file(path, relpath, routes):
     with open(path, "rt") as f:
@@ -24,26 +22,27 @@ def scan_file(path, relpath, routes):
             line = lines[i].rstrip()
             m = ROUTE_REGEX.search(line)
             if m:
-                key = m.group(1)
-                if not key:
-                    key = m.group(2)
-                if not key:
-                    continue
-                if key not in routes:
-                    routes[key] = []
-                fragment = line[m.start() + 1:]
-                j = fragment.find(',')
-                if j > -1:
-                    fragment = fragment[:j].strip()
-                routes[key].append((relpath, i + 1, fragment))
+                for i in range(1, 4):
+                    key = m.group(i)
+                    if key:
+                        if key not in routes:
+                            routes[key] = []
+                        fragment = line[m.start() + 1:]
+                        j = fragment.find(',')
+                        if j > -1:
+                            fragment = fragment[:j].strip()
+                        routes[key].append((relpath, i + 1, fragment))
 
 def print_routes(routes):
     keys = sorted([k for k in routes.keys()], key=lambda x: x[1:] if x[0] == '/' else x)
+    unique_paths = set()
     for key in keys:
         matches = routes[key]
         print(f"{key}: {len(matches)}")
         for match in matches:
-            print(f"    {match[0]}({match[1]}): {match[2]}") 
+            print(f"    {match[0]}({match[1]}): {match[2]}")
+            unique_paths.add(match[0])
+    print(f"----\n{len(keys)} unique routes in {len(unique_paths)} unique files")
 
 if __name__ == '__main__':
     target = sys.argv[1] if len(sys.argv) == 2 else None
